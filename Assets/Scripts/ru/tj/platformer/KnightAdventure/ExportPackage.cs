8d6fbd6e81dcb3494c0a04c3a18b73ab4526da79
tjomagavk <tjomagavk@yandex.ru>
2022-03-20 01:32:12 +0300

    Добавлен ExportPackage

---
+++ Assets/Scripts/ru/tj/platformer/KnightAdventure/ExportPackage.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace ru.tj.platformer.KnightAdventure {
+    public static class ExportPackage {
+        [MenuItem("Export/Export with tags and layers, Input settings")]
+        public static void export() {
+            string[] projectContent = {
+                "Packages/com.unity.cinemachine",
+                "Assets",
+                "ProjectSettings/TagManager.asset",
+                "ProjectSettings/InputManager.asset",
+                "ProjectSettings/ProjectSettings.asset",
+                "Packages/manifest.json",
+                "Packages/packages-lock.json"
+            };
+            AssetDatabase.ExportPackage(projectContent, "Done.unitypackage",
+                                        ExportPackageOptions.Interactive
+                                        | ExportPackageOptions.Recurse
+                                        | ExportPackageOptions.IncludeDependencies);
+            Debug.Log("Project Exported");
+        }
+    }
+}
